40bd26b25538661f203ce8e1a8f2e7d72a2cd8ba
Wijayarathna P.G.R.C <it18141948@my.sliit.lk>
2022-06-05 22:07:29 +0530

    Add swagger Dependanct

--- src/main/java/com/cloudofgoods/springsecurity/springSecurity/SpringSecurityApplication.java
+++ src/main/java/com/cloudofgoods/springsecurity/springSecurity/SpringSecurityApplication.java
@@ -14,10 +14,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringSecurityApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/cloudofgoods/springsecurity/springSecurity/security/WebSecurityConfig.java
+++ src/main/java/com/cloudofgoods/springsecurity/springSecurity/security/WebSecurityConfig.java
@@ -55,9 +55,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {/* WebSecur
         http.sessionManagement().sessionCreationPolicy(STATELESS);
         // Allow to access Request from Below any URLs
         http.authorizeRequests().antMatchers("/api/login/**","/api/token/refresh/**").permitAll();
+        http.authorizeRequests().antMatchers("/swagger-ui.html/**").permitAll();
         // Allow to access Request from Below URLs with having authorized Roles
         http.authorizeRequests().antMatchers(GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
+        // http.authorizeRequests().antMatchers(POST, "/api/user/update/**").hasAnyAuthority("ROLE_SUPER_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
         http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
